26e7ccd3874a1dbd5a8e109847f3b7a2f26cdcbd
synthwrkx <80101115+synthwrkx@users.noreply.github.com>
2023-06-19 19:35:52 +0530

    Add files via upload

--- DiseasePrediction/diseaseprediction.sql
+++ DiseasePrediction/diseaseprediction.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2023 at 04:25 PM
+-- Generation Time: Jun 19, 2023 at 04:04 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
